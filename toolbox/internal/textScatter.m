3228abb500ba8052e9e24cc59c70a8e4ddec5b31
José Camacho <josecamacho@ugr.es>
2026-03-24 20:21:00 +0100

    Update textScatter.m

--- toolbox/internal/textScatter.m
+++ toolbox/internal/textScatter.m
@@ -120,60 +120,95 @@ ax = axis;
 deltax = (ax(2)-ax(1)); 
 deltay = (ax(4)-ax(3)); 
 
-%bdata = bdata + deltax/100*randn(size(bdata,1),1)*deltay/100*randn(1,size(bdata,2));
 c = 0.01; % bias with text
 if ~isempty(elabel)
-    texti = []; 
     for i=1:N
         ind = [1:(i-1) (i+1):size(bdata,1)];
         
-        diffs = (bdata(ind,:)-bdata(i,:))./[deltax/sqrt(4) deltay/sqrt(3)]; 
+        diffs = (bdata(ind,:)-bdata(i,:))./[deltax/sqrt(4) deltay/sqrt(3)]; % This creates a set of neighbours closer to the blur index
         dist_vec = sqrt(sum(diffs.^2, 2));
-        knn = find(dist_vec < blur);
+        knn = find(dist_vec < blur); 
         extr = [length(find(diffs(knn,1)>= 0)) length(find(diffs(knn,1)<= 0)) length(find(diffs(knn,2)>= 0)) length(find(diffs(knn,2)<= 0))]; % none right, none left, none up, none down 
-        if blur==0 | length(knn)==0 | any(extr==0) % either no blur control activated, no close neighbour or a extreme point
-            texti(end+1) = i;
+        if blur==0 | length(knn)==0 | any(extr==0) % either no blur control activated, no close neighbour or a extreme point wiht neigjbours
+
+            posx = bdata(i,1) + c*deltax;
+            posy = bdata(i,2) + c*deltay;
+            halign = 'left';
+            valign = 'bottom';
+            bias = -mean(diffs(knn,:),1);
+    
+            % Label position based on cuadrant & interference
+            if ~isempty(knn) % if there are neighbours (interference), locate from them
+                if bias(1) < 0 
+                    posx = bdata(i,1) - c*deltax;
+                    halign = 'right';
+                end
+                if bias(2) < 0
+                    posy = bdata(i,2) - c*deltay;
+                    valign = 'top';
+                end
+            else % otherwise locate from que quadrant
+                if bdata(i,1) < 0 
+                    posx = bdata(i,1) - c*deltax;
+                    halign = 'right';
+                end
+                if bdata(i,2) < 0
+                    posy = bdata(i,2) - c*deltay;
+                    valign = 'top';
+                end
+            end
+    
+            if any(strcmp(plottype,{'zaxis','zshape'}))
+                t = text(posx, posy, mult(i), strtrim(elabel(i,1)),'VerticalAlignment',valign, 'HorizontalAlignment',halign,'FontSize', 12, 'Interpreter', 'none');
+            elseif strcmp(plottype,'zsize')
+                t = text(posx, posy, ord_classes(i), strtrim(elabel(i,1)),'VerticalAlignment',valign, 'HorizontalAlignment',halign,'FontSize', 12, 'Interpreter', 'none');
+            else
+                t = text(posx, posy, strtrim(elabel(i,1)),'VerticalAlignment',valign, 'HorizontalAlignment',halign,'FontSize', 12, 'Interpreter', 'none');
+            end
+            
+            ext = t.Extent; % entend axis if necessary
+            text_right_edge = ext(1) + ext(3);
+            text_top_edge   = ext(2) + ext(4);
+            ax(1) = min(ax(1), ext(1)/1.1);
+            ax(2) = max(ax(2), 1.1*text_right_edge);
+            ax(3) = min(ax(3), ext(2)/1.1);
+            ax(4) = max(ax(4), 1.1*text_top_edge);
+            axis(ax);
         end
     end
 
-    for ii=1:length(texti)
-        i = texti(ii);
-        indt = [1:(ii-1) (ii+1):length(texti)];
-        diffst = (bdata(texti(indt),:)-bdata(i,:))./[deltax/sqrt(4) deltay/sqrt(3)]; 
-        dist_vect = sqrt(sum(diffst.^2, 2));
-        knnt = find(dist_vect < blur);
+    txt_objs = findobj(gca, 'Type', 'text');
+    n = length(txt_objs);
+    padding = 0; % Extra pixels to keep them apart
+    iterations = 2; % Repeat a few times to resolve secondary collisions
 
-        posx = bdata(i,1) + c*deltax;
-        posy = bdata(i,2) + c*deltay;
-        halign = 'left';
-        valign = 'bottom';
-        bias = -mean(diffst(knnt,:),1);
+    for iter = 1:iterations % Do some additional iterations to separate labels
+        for i = 1:n
+            for j = i+1:n
+                % Get current positions
+                ext1 = txt_objs(i).Extent;
+                ext2 = txt_objs(j).Extent;
 
-        % Label position based on cuadrant & interference
-        if bias(1) < 0 
-            posx = bdata(i,1) - c*deltax;
-            halign = 'right';
-        end
-        if bias(2) < 0
-            posy = bdata(i,2) - c*deltay;
-            valign = 'top';
-        end
+                % Calculate overlap in X and Y
+                overlapX = min(ext1(1)+ext1(3), ext2(1)+ext2(3)) - max(ext1(1), ext2(1));
+                overlapY = min(ext1(2)+ext1(4), ext2(2)+ext2(4)) - max(ext1(2), ext2(2));
 
-        if any(strcmp(plottype,{'zaxis','zshape'}))
-            t = text(posx, posy, mult(i), strtrim(elabel(i,1)),'VerticalAlignment',valign, 'HorizontalAlignment',halign,'FontSize', 12, 'Interpreter', 'none');
-        elseif strcmp(plottype,'zsize')
-            t = text(posx, posy, ord_classes(i), strtrim(elabel(i,1)),'VerticalAlignment',valign, 'HorizontalAlignment',halign,'FontSize', 12, 'Interpreter', 'none');
-        else
-            t = text(posx, posy, strtrim(elabel(i,1)),'VerticalAlignment',valign, 'HorizontalAlignment',halign,'FontSize', 12, 'Interpreter', 'none');
+                % If both are positive, they overlap
+                if overlapX > 0 && overlapY > 0
+                    % Move along the smaller overlap to minimize distance
+                    if overlapX < overlapY
+                        moveX = (overlapX + padding) / 2;
+                        signX = sign(ext1(1) - ext2(1));
+                        txt_objs(i).Position(1) = txt_objs(i).Position(1) + signX * moveX;
+                        txt_objs(j).Position(1) = txt_objs(j).Position(1) - signX * moveX;
+                    else
+                        moveY = (overlapY + padding) / 2;
+                        signY = sign(ext1(2) - ext2(2));
+                        txt_objs(i).Position(2) = txt_objs(i).Position(2) + signY * moveY;
+                        txt_objs(j).Position(2) = txt_objs(j).Position(2) - signY * moveY;
+                    end
+                end
+            end
         end
-        
-        ext = t.Extent; 
-        text_right_edge = ext(1) + ext(3);
-        text_top_edge   = ext(2) + ext(4);
-        ax(1) = min(ax(1), ext(1));
-        ax(2) = max(ax(2), text_right_edge);
-        ax(3) = min(ax(3), ext(2));
-        ax(4) = max(ax(4), text_top_edge);
-        axis(ax);
     end
 end
